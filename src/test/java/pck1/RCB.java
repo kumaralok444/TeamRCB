534792bb27b8967a38ac86f9fa06e82df1f9e18e
Alok Kumar <kumaralok444@gmail.com>
2023-01-29 22:21:17 +0530

    Intial Commit adding json file and java class.

---
+++ src/test/java/pck1/RCB.java
@@ -0,0 +1,53 @@
+package pck1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class RCB {
+	public static String resourceLocation = System.getProperty("user.dir").toString().replace("\\", "\\\\")
+			+ "\\src\\test\\resources\\";
+	Object obj;
+	JSONObject jo;
+	@BeforeSuite
+	public void beforeSuite() throws FileNotFoundException, IOException, ParseException {
+		obj = new JSONParser().parse(new FileReader(resourceLocation+"\\TeamRCB.json"));
+		jo = (JSONObject) obj;
+
+	}
+@Test
+public void tc1() {
+	int count=0;
+	JSONArray ja = (JSONArray) jo.get("player");
+	for(Object ob:ja) {
+		String st=(String)(((JSONObject) ob).get("country"));
+		if(!st.equalsIgnoreCase("India")) {
+			count++;
+		}
+	}
+	Assert.assertEquals(count, 4);
+	//System.out.println("Test Here");
+}
+@Test
+public void tc2() {
+	boolean flag=false;
+	JSONArray ja = (JSONArray) jo.get("player");
+	for(Object ob:ja) {
+		String st=(String)(((JSONObject) ob).get("role"));
+		if(st.equalsIgnoreCase("Wicket-keeper")) {
+			flag=true;
+		}
+	}
+	Assert.assertTrue(flag);
+	//System.out.println("Test Here");
+}
+}
